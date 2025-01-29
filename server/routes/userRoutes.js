075132b297aa4b5eff9353cf090e562d5532e7f5
Karmavir Joshi <karmavirj@live.com>
2025-01-29 12:04:10 +0530

    removed some code

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,7 @@
 import express from "express";
-import { userProtectBearer } from "../middlewares/userProtect.js";
 import * as userController from "../controllers/userController.js";
 
 const router = express.Router();
-
-router.post("/login", userController.loginUser);
 
 router.post("/create", userController.createUser); // create a new user
 
