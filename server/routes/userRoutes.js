328bbd597f2df3cd484536176d4d4fd6f793b28f
Karmavir Joshi <karmavirj@live.com>
2025-01-29 11:39:39 +0530

    added userController and userRoutes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { userProtectBearer, userProtectCookie } from "../middlewares/userProtect.js";
 
-import { createUser,listUsers, getUser, loginUser } from "../controllers/userController.js";
+import { userProtectBearer } from "../middlewares/userProtect.js";
+
+import * as userController from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.post("/login", loginUser);
+router.post("/login", userController.loginUser);
 
-router.post("/create", createUser); // create a new user
+router.post("/create", userController.createUser); // create a new user
 
 
 export default router;
